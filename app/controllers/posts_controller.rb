96039bf4da015d0ae67ec53d1eabdfac02dcd159
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-10-03 17:35:42 +0900

    再修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: :index
   def index
-    @posts = Post.order("id DESC").page(params[:page]).per(8)
+    @posts = Post.includes(:user).page(params[:page]).per(8).order("created_at DESC")
   end
 
   def new
@@ -14,12 +14,16 @@ class PostsController < ApplicationController
     # Post.create(title: post_params[:title], text: post_params[:text])
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:title, :text).merge(user_id: current_user.id)
     # params.permit(:title, :text)
   end
 end

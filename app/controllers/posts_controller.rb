5a9e0f2adfee034300445fabf0be0ed96d513d56
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-10-03 20:52:50 +0900

    all commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :move_to_index, except: :index
+  # before_action :move_to_index, except: :index
   def index
     @posts = Post.includes(:user).page(params[:page]).per(8).order("created_at DESC")
   end
@@ -14,14 +14,32 @@ class PostsController < ApplicationController
     # Post.create(title: post_params[:title], text: post_params[:text])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.update(post_params)
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.destroy
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
   private
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in?
+  # end
   def post_params
     params.require(:post).permit(:title, :text).merge(user_id: current_user.id)
     # params.permit(:title, :text)

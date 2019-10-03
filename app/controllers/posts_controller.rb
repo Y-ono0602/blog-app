f75ac725e179060658ad6ab637098db5b853adaa
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-10-03 16:05:11 +0900

    投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
+    @posts = Post.order("id DESC").page(params[:page]).per(8)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    # Post.create(title: post_params[:title], text: post_params[:text])
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+  def post_params
+    params.require(:post).permit(:title, :text)
+    # params.permit(:title, :text)
   end
 end
